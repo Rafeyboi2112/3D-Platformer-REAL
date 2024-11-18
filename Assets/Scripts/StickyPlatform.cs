0d886b472cf10324420a8321045cd4fece505805
Rafeyboi2112 <rafe.lepherd@gmail.com>
2024-11-18 15:02:42 +0000

    Sticky wicky

--- Assets/Scripts/StickyPlatform.cs
+++ Assets/Scripts/StickyPlatform.cs
@@ -4,19 +4,40 @@ using UnityEngine;
 
 public class StickyPlatform : MonoBehaviour
 {
+    private Vector3 lastPlatformPosition;
+    private GameObject player;
+
+    private void Start()
+    {
+        lastPlatformPosition = transform.position;
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.name == "Player")
         {
-            collision.gameObject.transform.SetParent(transform);
+            player = collision.gameObject;
+            lastPlatformPosition = transform.position;
         }
     }
 
     private void OnCollisionExit(Collision collision)
     {
-        if (collision.gameObject.name == "Player")
+        if(collision.gameObject.name == "Player")
         {
-            collision.gameObject.transform.SetParent(null);
+            player = null;
         }
+    }
+
+    private void Update()
+    {
+        if (player != null)
+        {
+            Vector3 platformMovement = transform.position - lastPlatformPosition;
+
+            player.transform.position += platformMovement;
+        }
+
+        lastPlatformPosition = transform.position;
     }
 }
